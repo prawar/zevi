5d5bd99442dfe91086a918f91a02f5672d7b7387
prawar <prawarmundra20@gmail.com>
2023-01-08 23:59:30 +0530

    basic search page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../styles/Home.css'
+import Model from './Model';
 
 const Home = () => {
+  let inputRef = useRef(null);
+  const [show, setShow] = useState(false);
+
+  const handleSearch = () => {
+    let value = inputRef.current.value;
+    console.log(value);
+  }
+
   return (
     <div className='Home'>
       <div className='input'>
-        <input placeholder='Search' type='text'/>
-        <img src='/search.svg' className='search-icon' />
+        <input 
+          className='home-input'
+          ref={inputRef}
+          placeholder='Search'
+          type='text'
+          onKeyDown={e => {
+            if(e.key === 'Enter')
+            handleSearch();
+          }}
+          onFocus={()=>setShow(true)}
+          // onBlur={()=>setShow(false)}/
+          />
+        <img src='/search.svg' className='search-icon' onClick={handleSearch} />
       </div>
       <img src='/ZEVI.svg' alt='logo' className='logo' />
+      {show&&<Model/>}
     </div>
   )
 }
 
-export default Home
+export default Home;
